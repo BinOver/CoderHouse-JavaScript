adc41f89444ea383097dc92fe2c0931a4cb9a82f
binover <binover@gmail.com>
2023-04-03 12:37:12 -0300

    Segunda entrega

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,5 @@
-function validarEjercico (){
+//Funcion que valida el ingreso de ejercio
+function validarMusculo (){
     let ejercicio = prompt('Ingrece ejercio:\n-Pecho\n-Espalda\n-Brazos\n-Piernas').toLowerCase();
     while (ejercicio !='pecho' && ejercicio != 'espalda' && ejercicio !='brazos' && ejercicio != 'piernas'){
         ejercicio = prompt('Ejercicio inexistente!\nIngrece ejercio:\n-Pecho\n-Espalda\n-Brazos\n-Piernas').toLowerCase();
@@ -6,27 +7,112 @@ function validarEjercico (){
     return ejercicio;
 }
 
-function darRutina (ejercicio){
-    switch(ejercicio){
-        case 'pecho':
-            alert('Banco plano con barra: 10x3\nBanco inclinado c/mancuerna: 10x3\nPecho c/polea: 10x3');
-            break;
-        case 'espalda':
-            alert('Remo con polea 10x3\nDorsalera: 10x3\nSerrucho c/mancuerna: 12x4');
-            break;
-        case 'brazos':
-            alert('Biceps W: 10x3\nBiceps Martillo: 10x3\nTriceps pull over: 12x4');
-            break;
-        case 'piernas':
-            alert('Sentadillas 10x3\nPrensa: 12x4\nCalf 12x4');
-            break;
-    }
-
+function validarCantidad (){
+    let cant = 0;
+    do{
+        cant = prompt('Ingrese la cantidad maxima de ejercicios');
+        if (cant < 0 ){
+            alert('La cantidad debe ser mayor a 0');
+        }
+    }while(cant < 0);
+    return cant;
 }
 
 
-let ejercicio = validarEjercico();
-darRutina(ejercicio);
+// Funcion que devuelve un array con los ejecicios para el musculo seleccionado
+function darRutina (ejercicios,musculo,cantidad){
+    const rutina = ejercicios.filter(eje => eje.musculo == musculo);
+    if (cantidad > rutina.leght){
+        return rutina
+    }else{
+        return rutina.slice(0,cantidad);
+    }
+}
+
+// Funcion que muestra la rutina al usuario
+function mostrarRutina (rutina){
+    for (const rut of rutina) {
+        console.table(rut);
+    }
+}
+
+// Array de datos
+const ejercicios = [
+    {
+        musculo:"pecho",
+        ejercicio:"Banco Plano c/barra",
+        repeticiones:10,
+        series:3
+    },
+    {
+        musculo:"pecho",
+        ejercicio:"Banco inclinado c/mancuerna",
+        repeticiones:15,
+        series:4
+    },
+    {
+        musculo:"pecho",
+        ejercicio:"Polea",
+        repeticiones:10,
+        series:4
+    },
+    {
+        musculo:"espalda",
+        ejercicio:"Remo c/polea",
+        repeticiones:15,
+        series:4
+    },
+    {
+        musculo:"espalda",
+        ejercicio:"Biceps Martilo",
+        repeticiones:10,
+        series:3
+    },
+    {
+        musculo:"espalda",
+        ejercicio:"Serrucho c/mancuerna",
+        repeticiones:12,
+        series:4
+    },
+    {
+        musculo:"brazos",
+        ejercicio:"Biceps W",
+        repeticiones:10,
+        series:3
+    },
+    {
+        musculo:"brazos",
+        ejercicio:"Biceps Martillo",
+        repeticiones:10,
+        series:3
+    },
+    {
+        musculo:"brazos",
+        ejercicio:"Pull Over",
+        repeticiones:12,
+        series:4
+    },
+    {
+        musculo:"piernas",
+        ejercicio:"Sentadilla",
+        repeticiones:10,
+        series:3
+    },
+    {
+        musculo:"piernas",
+        ejercicio:"Prensa",
+        repeticiones:12,
+        series:4
+    },
+    {
+        musculo:"piernas",
+        ejercicio:"Calf",
+        repeticiones:15,
+        series:4
+    }
+]
+
+mostrarRutina(darRutina(ejercicios,validarMusculo(),validarCantidad()));
 
 
 
