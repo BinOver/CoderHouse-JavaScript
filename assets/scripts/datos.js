476e621487e85ce6686f7c4a27b4226360cd4635
binover <binover@gmail.com>
2023-04-13 19:23:31 -0300

    DOM

---
+++ assets/scripts/datos.js
@@ -0,0 +1,87 @@
+// Array de Objetos con los datos de los ejecicios
+const ejercicios = [
+    {
+        musculo:"pecho",
+        ejercicio:"Banco Plano c/barra",
+        repeticiones:10,
+        series:3,
+        image:"../assets/images/pecho.webp"
+    },
+    {
+        musculo:"pecho",
+        ejercicio:"Banco inclinado c/mancuerna",
+        repeticiones:15,
+        series:4,
+        image:"../assets/images/pecho.webp"
+    },
+    {
+        musculo:"pecho",
+        ejercicio:"Polea",
+        repeticiones:10,
+        series:4,
+        image:"../assets/images/pecho.webp"
+    },
+    {
+        musculo:"espalda",
+        ejercicio:"Remo c/polea",
+        repeticiones:15,
+        series:4,
+        image:"../assets/images/espalda.jpeg"
+    },
+    {
+        musculo:"espalda",
+        ejercicio:"Biceps Martilo",
+        repeticiones:10,
+        series:3,
+        image:"../assets/images/espalda.jpeg"
+    },
+    {
+        musculo:"espalda",
+        ejercicio:"Serrucho c/mancuerna",
+        repeticiones:12,
+        series:4,
+        image:"../assets/images/espalda.jpeg"
+    },
+    {
+        musculo:"brazos",
+        ejercicio:"Biceps W",
+        repeticiones:10,
+        series:3,
+        image:"../assets/images/brazos.webp"
+    },
+    {
+        musculo:"brazos",
+        ejercicio:"Biceps Martillo",
+        repeticiones:10,
+        series:3,
+        image:"../assets/images/brazos.webp"
+    },
+    {
+        musculo:"brazos",
+        ejercicio:"Pull Over",
+        repeticiones:12,
+        series:4,
+        image:"../assets/images/brazos.webp"
+    },
+    {
+        musculo:"piernas",
+        ejercicio:"Sentadilla",
+        repeticiones:10,
+        series:3,
+        image:"../assets/images/piernas.jpg"
+    },
+    {
+        musculo:"piernas",
+        ejercicio:"Prensa",
+        repeticiones:12,
+        series:4,
+        image:"../assets/images/piernas.jpg"
+    },
+    {
+        musculo:"piernas",
+        ejercicio:"Calf",
+        repeticiones:15,
+        series:4,
+        image:"../assets/images/piernas.jpg"
+    }
+]
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,28 +1,20 @@
-//Funcion que valida el ingreso de ejercio
-function validarMusculo (){
-    let ejercicio = prompt('Ingrece ejercio:\n-Pecho\n-Espalda\n-Brazos\n-Piernas').toLowerCase();
-    while (ejercicio !='pecho' && ejercicio != 'espalda' && ejercicio !='brazos' && ejercicio != 'piernas'){
-        ejercicio = prompt('Ejercicio inexistente!\nIngrece ejercio:\n-Pecho\n-Espalda\n-Brazos\n-Piernas').toLowerCase();
-    }
-    return ejercicio;
-}
-
 //Funcion que valida la cantidad de ejercicios requerida
 function validarCantidad (){
-    let cant = 0;
-    do{
-        cant = Math.floor(prompt('Ingrese la cantidad maxima de ejercicios'));
-        if (cant < 0 ){
-            alert('La cantidad debe ser mayor a 0');
-        }
-    }while(cant < 0);
+    // let cant = 0;
+    let c = document.getElementById('cant');
+    let cant = parseInt(c.value);
+    if (cant <= 0 ){
+        alert('La cantidad debe ser mayor a 0');
+        return 0;
+    }
+    console.log(c);
     return cant;
 }
 
 // Funcion que devuelve un array con los ejecicios para el musculo seleccionado teniendo en cuenta la cantidad
 function darRutina (ejercicios,musculo,cantidad){
     const rutina = ejercicios.filter(eje => eje.musculo == musculo);
-    if (cantidad > rutina.leght){
+    if (cantidad > rutina.length){
         return rutina
     }else{
         return rutina.slice(0,cantidad);
@@ -31,89 +23,42 @@ function darRutina (ejercicios,musculo,cantidad){
 
 // Funcion que muestra la rutina al usuario. IMPORTANTE: los resultados se muestran por Console.table 
 function mostrarRutina (rutina){
+    let tarjeta = document.getElementById('tarjetas');
+    tarjeta.innerHTML = '';
+
     for (const rut of rutina) {
         console.table(rut);
+        tarjeta.innerHTML += `
+        <div class="col-3 card m-2 p-0">
+            <img class="card-img-top" src="${rut.image}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${(rut.musculo).toUpperCase()}</h5>
+                <p class="card-text">Ejercicio: ${rut.ejercicio}</p>
+                <p class="card-text">Repeticiones: ${rut.repeticiones}</p>
+                <p class="card-text">Series: ${rut.series}</p>
+            </div>
+        </div>
+        `
     }
 }
 
-// Array de Objetos con los datos de los ejecicios
-const ejercicios = [
-    {
-        musculo:"pecho",
-        ejercicio:"Banco Plano c/barra",
-        repeticiones:10,
-        series:3
-    },
-    {
-        musculo:"pecho",
-        ejercicio:"Banco inclinado c/mancuerna",
-        repeticiones:15,
-        series:4
-    },
-    {
-        musculo:"pecho",
-        ejercicio:"Polea",
-        repeticiones:10,
-        series:4
-    },
-    {
-        musculo:"espalda",
-        ejercicio:"Remo c/polea",
-        repeticiones:15,
-        series:4
-    },
-    {
-        musculo:"espalda",
-        ejercicio:"Biceps Martilo",
-        repeticiones:10,
-        series:3
-    },
-    {
-        musculo:"espalda",
-        ejercicio:"Serrucho c/mancuerna",
-        repeticiones:12,
-        series:4
-    },
-    {
-        musculo:"brazos",
-        ejercicio:"Biceps W",
-        repeticiones:10,
-        series:3
-    },
-    {
-        musculo:"brazos",
-        ejercicio:"Biceps Martillo",
-        repeticiones:10,
-        series:3
-    },
-    {
-        musculo:"brazos",
-        ejercicio:"Pull Over",
-        repeticiones:12,
-        series:4
-    },
-    {
-        musculo:"piernas",
-        ejercicio:"Sentadilla",
-        repeticiones:10,
-        series:3
-    },
-    {
-        musculo:"piernas",
-        ejercicio:"Prensa",
-        repeticiones:12,
-        series:4
-    },
-    {
-        musculo:"piernas",
-        ejercicio:"Calf",
-        repeticiones:15,
-        series:4
+//Funcion que trae el valor del Radio 
+function obtenerMusculo(){
+    let musc = document.getElementsByName('musculo');
+    for (const m of musc){
+        if(m.checked){
+            console.log(m.value);
+            return m.value;
+        }
     }
-]
+}
 
 // Ejecucion
-mostrarRutina(darRutina(ejercicios,validarMusculo(),validarCantidad()));
+
+function pedirRutina(){
+    mostrarRutina(darRutina(ejercicios,obtenerMusculo(),validarCantidad()))
+};
+
 
 
 
